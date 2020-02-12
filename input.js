dd8b687ca53a1e3898201dbffda442d75b8be02c
NoahNovick <nnovickfaille1@gmail.com>
2020-02-12 14:02:58 -0500

    move function added

--- input.js
+++ input.js
@@ -1,16 +1,33 @@
-const handleUserInput = key => {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+
   return stdin;
+};
+
+const handleUserInput = key => {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+
+  if (key === "d") {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = { setupInput };
