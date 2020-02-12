638797c4de07650dcc429245a8ad861589a5cfc8
NoahNovick <nnovickfaille1@gmail.com>
2020-02-12 14:49:09 -0500

    cleaned up using exports

--- client.js
+++ client.js
@@ -1,8 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
+
 const connect = function() {
   const conn = net.createConnection({
-    host: "172.46.0.236",
-    port: 50541
+    host: IP,
+    port: PORT
   });
   conn.setEncoding("utf8");
 
