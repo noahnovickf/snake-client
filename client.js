406aa6f99ccab555f9023c8cfabd1615cfa45b36
NoahNovick <nnovickfaille1@gmail.com>
2020-02-12 12:50:27 -0500

    created another folder and required our function to clean up code

---
+++ client.js
@@ -0,0 +1,14 @@
+const net = require("net");
+const connect = function() {
+  const conn = net.createConnection({
+    host: "172.46.0.236",
+    port: 50541
+  });
+  conn.setEncoding("utf8");
+
+  conn.on("data", data => {
+    console.log(data);
+  });
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,16 +1,5 @@
 const net = require("net");
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "172.46.0.236",
-    port: 50541
-  });
-  conn.setEncoding("utf8");
-
-  conn.on("data", data => {
-    console.log(data);
-  });
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
